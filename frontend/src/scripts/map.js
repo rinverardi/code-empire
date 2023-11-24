cfe0457c3ccfacc92c29542edefc3998e272305e
Remo Inverardi <remo@inverardi.ch>
2023-11-24 17:41:16 +0100

    Improve the method names of the map view.

--- frontend/src/scripts/map.js
+++ frontend/src/scripts/map.js
@@ -9,19 +9,19 @@ export class MapTile {
 }
 
 export class MapView {
-    #addMap(tiles) {
+    #buildMap(tiles) {
         const mapElement = document.createElement('div');
 
         mapElement.id = 'map';
 
         for (let y = 0; y < tiles.length; y++) {
-            const rowElement = this.#addRow();
+            const rowElement = this.#buildRow();
 
             for (let x = 0; x < tiles[y].length; x++) {
                 const tile = tiles[y][x];
 
                 if (tile !== ' ') {
-                    const tileElement = this.#addTile(tile, x, y);
+                    const tileElement = this.#buildTile(tile, x, y);
 
                     rowElement.appendChild(tileElement);
                 }
@@ -33,7 +33,7 @@ export class MapView {
         return mapElement;
     }
 
-    #addRow() {
+    #buildRow() {
         const rowElement = document.createElement('div');
 
         rowElement.classList = ['map-row'];
@@ -41,7 +41,7 @@ export class MapView {
         return rowElement;
     }
 
-    #addTile(tile, x, y) {
+    #buildTile(tile, x, y) {
         const tileElement = document.createElement('div');
 
         tileElement.classList.add('tile');
@@ -65,7 +65,7 @@ export class MapView {
         let mapElement = document.getElementById('map');
 
         if (!mapElement) {
-            mapElement = this.#addMap(game.map.tiles);
+            mapElement = this.#buildMap(game.map.tiles);
 
             document.getElementById('map-container').appendChild(mapElement);
         }
