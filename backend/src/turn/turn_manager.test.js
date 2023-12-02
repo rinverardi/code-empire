648589799bae2118a488b57a869feb0251fd5f8a
Remo Inverardi <remo@inverardi.ch>
2023-12-02 12:36:22 +0100

    Test the turn manager.

---
+++ backend/src/turn/turn_manager.test.js
@@ -0,0 +1,106 @@
+import { GlobalContext } from "../lib/global_context.mjs";
+import { Turn } from "./turn.mjs";
+
+class TestContext extends GlobalContext {
+    gameAccess() {
+        return {
+            getCurrentPlayer: (game) => ({
+                position: [2, 1]
+            })
+        };
+    }
+}
+
+test('Can move', () => {
+    const testContext = new TestContext();
+
+    const game = {
+        map: {
+            tiles: [
+                ' g g ',
+                'g g g',
+                ' g g '
+            ]
+        },
+        players: []
+    };
+
+    testContext.turnManager().startTurn(game);
+
+    // The player can move in all directions.
+
+    const turns = game.turns.filter(that => that.type === Turn.Type.move);
+
+    expect(turns.length).toBe(6);
+});
+
+test('Cannot step on opponent', () => {
+    const testContext = new TestContext();
+
+    const game = {
+        map: {
+            tiles: [
+                ' g g ',
+                'g g g',
+                ' g g '
+            ]
+        },
+        players: [{
+            position: [0, 1]
+        }]
+    };
+
+    testContext.turnManager().startTurn(game);
+
+    // The player can move in all directions but west.
+
+    const turns = game.turns.filter(that => that.type === Turn.Type.move);
+
+    expect(turns.length).toBe(5);
+});
+
+test('Cannot walk on mountain', () => {
+    const textContext = new TestContext();
+
+    const game = {
+        map: {
+            tiles: [
+                ' g g ',
+                'm g g',
+                ' g g '
+            ]
+        },
+        players: []
+    };
+
+    textContext.turnManager().startTurn(game);
+
+    // The player can move in all directions but west.
+
+    const turns = game.turns.filter(that => that.type === Turn.Type.move);
+
+    expect(game.turns.length).toBe(5);
+});
+
+test('Cannot walk on water', () => {
+    const testContext = new TestContext();
+
+    const game = {
+        map: {
+            tiles: [
+                ' g g ',
+                'w g g',
+                ' g g '
+            ]
+        },
+        players: []
+    };
+
+    testContext.turnManager().startTurn(game);
+
+    // The player can move in all directions but west.
+
+    const turns = game.turns.filter(that => that.type === Turn.Type.move);
+
+    expect(game.turns.length).toBe(5);
+});
