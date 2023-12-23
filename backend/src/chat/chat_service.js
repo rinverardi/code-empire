a879a1f22e5c76661c7460a753626ba52d735177
Remo Inverardi <remo@inverardi.ch>
2023-12-23 15:31:24 +0100

    Enable the authn/authz for the chat service.

--- backend/src/chat/chat_service.js
+++ backend/src/chat/chat_service.js
@@ -1,15 +1,19 @@
 export class ChatService {
+    #authn;
+    #authz;
     #gameRepository;
 
     constructor(globalContext) {
+        this.#authn = globalContext.authn();
+        this.#authz = globalContext.authz();
         this.#gameRepository = globalContext.gameRepository();
     }
 
     async sendMessage(sessionContext, text) {
-
-        // TODO Check the status!
-
         const game = await this.#gameRepository.loadGame(sessionContext);
+        const player = this.#authn.getPlayer(sessionContext, game);
+
+        this.#authz.canSendMessage(game, player).orThrow();
 
         game.messages.push({
             player: sessionContext.playerId,
