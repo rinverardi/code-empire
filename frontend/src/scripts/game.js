f9f9e7418e7c4285208182f6bdf334e736dacbc2
Remo Inverardi <remo@inverardi.ch>
2023-12-23 09:36:17 +0100

    Remove the 'executing' game status.

--- frontend/src/scripts/game.js
+++ frontend/src/scripts/game.js
@@ -8,7 +8,6 @@ export class Game {
     static get Status() {
         return Object.freeze({
             aborted: 'aborted',
-            executing: 'executing',
             missing: 'missing',
             thinking: 'thinking',
             waiting: 'waiting'
