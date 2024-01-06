ea57a9678fd395a09df0f7224fd29c7d539ba811
Remo Inverardi <remo@inverardi.ch>
2024-01-06 21:53:20 +0100

    Implement attacks in the frontend.

--- frontend/src/scripts/notification.js
+++ frontend/src/scripts/notification.js
@@ -1,5 +1,14 @@
 import { Html } from './util.js';
 
+export class Notification {
+    static get Type() {
+        return Object.freeze({
+            attack: 'attack',
+            kill: 'kill'
+        });
+    }
+}
+
 export class NotificationHelper {
     #notifications = [];
 
@@ -76,8 +85,46 @@ export class NotificationView {
             this.#notificationHelper.clear();
         }
 
+        for (const notification of game.notifications) {
+            switch (notification.type) {
+                case Notification.Type.attack:
+                    this.#showAttack(game, notification);
+                    break;
+
+                case Notification.Type.kill:
+                    this.#showKill(game, notification);
+                    break;
+
+                default:
+                    throw new RangeError('No such notification');
+            }
+        }
+
         this.#showCurrentPlayer(game);
         this.#showCurrentMessage(game);
+    }
+
+    #showAttack(game, notification) {
+        const attacker = this.#playerHelper.getPlayer(game, notification.attacker);
+        const victim = this.#playerHelper.getPlayer(game, notification.victim);
+
+        var message;
+
+        switch (this.#playerHelper.loadId()) {
+            case attacker.id:
+                message = `Du attackierst ${Html.escape(victim.name)}.`;
+                break;
+
+            case victim.id:
+                message = `${Html.escape(attacker.name)} attackiert dich!`;
+                break;
+
+            default:
+                message = `${Html.escape(attacker.name)} attackiert ${Html.escape(victim.name)}.`;
+                break;
+        }
+
+        this.#notificationHelper.showInformation(`${message}<br>(noch ${Html.escape(victim.health)} Trefferpunkte)`);
     }
 
     #showCurrentMessage(game) {
@@ -108,5 +155,28 @@ export class NotificationView {
                 this.#notificationHelper.showInformation(`${playerName} ist am Zug.`);
             }
         }
+    }
+
+    #showKill(game, notification) {
+        const attacker = this.#playerHelper.getPlayer(game, notification.attacker);
+        const victim = this.#playerHelper.getPlayer(game, notification.victim);
+
+        var message;
+
+        switch (this.#playerHelper.loadId()) {
+            case attacker.id:
+                message = `Du hast ${Html.escape(victim.name)} eliminiert.`;
+                break;
+
+            case victim.id:
+                message = `${Html.escape(attacker.name)} hat dich eliminiert!`;
+                break;
+
+            default:
+                message = `${Html.escape(attacker.name)} hat ${Html.escape(victim.name)} eliminiert.`;
+                break;
+        }
+
+        this.#notificationHelper.showInformation(message);
     }
 }
