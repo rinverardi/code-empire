584c2cc354bdd917d3d68bea5ac4422fbbb2c70b
Remo Inverardi <remo@inverardi.ch>
2024-01-13 17:00:58 +0100

    Clean up.

--- backend/src/resource/resource.js
+++ backend/src/resource/resource.js
@@ -1,9 +1,3 @@
-export class Resource {
-    static get Constants() {
-        return Object.freeze({
-            respawnTime: 9
-        });
-    }
 
     static get Type() {
         return Object.freeze({
