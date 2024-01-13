0e5dc6c81aafcb6c44f5e1c19befddd8774ddaf4
Remo Inverardi <remo@inverardi.ch>
2024-01-13 11:41:31 +0100

    Document the controller classes of the backend.

--- backend/src/game/game_controller.js
+++ backend/src/game/game_controller.js
@@ -1,5 +1,17 @@
 import { Action } from '../action/action.js';
+import { GlobalContext } from '../lib/global_context.js';
 import { Logger } from '../lib/logger.js';
+import { SessionContext } from '../lib/session_context.js';
+
+/**
+ * Acts as an interface between the WebSocket connection and the game logic:
+ *
+ * <ul>
+ *   <li>When the connection is opened, it is tied to one or more game states.
+ *   <li>Inbound messages are used to update the game states.
+ *   <li>Outbound messages notify clients about changes to the game states.
+ * </ul>
+ */
 
 export class GameController {
     #chatService;
@@ -7,12 +19,28 @@ export class GameController {
     #playerService;
     #turnService;
 
+    /**
+     * Avoid calling this constructor directly! Instead, use the globally-scoped
+     * object from the global context.
+     *
+     * @param {GlobalContext} globalContext holds the globally-scoped objects
+     */
+
     constructor(globalContext) {
         this.#chatService = globalContext.chatService();
         this.#gameService = globalContext.gameService();
         this.#playerService = globalContext.playerService();
         this.#turnService = globalContext.turnService();
     }
+
+    /**
+     * Manages the communication related to the current game.
+     * <p>
+     * Used both in the pre-game-phase (i.e., during match making) and in the
+     * in-game-phase (i.e., while actually playing).
+     *
+     * @param {SessionContext} sessionContext holds the session-scoped objects
+     */
 
     async watchGame(sessionContext) {
         const wsConnection = sessionContext.wsConnection();
@@ -70,6 +98,14 @@ export class GameController {
             wsConnection.send(JSON.stringify(game));
         });
     }
+
+    /**
+     * Manages the communication for all games.
+     * <p>
+     * Only used in the pre-game-phase (i.e., during match making).
+     *
+     * @param {SessionContext} sessionContext holds the session-scoped objects
+     */
 
     async watchGameList(sessionContext) {
         const wsConnection = sessionContext.wsConnection();
